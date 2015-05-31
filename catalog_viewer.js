300c241669f8dc42e703ab04a2f848f1d8412e4e
Raphaël Pinson <raphink@gmail.com>
2015-05-31 22:36:28 +0200

    Loading data: progress bar

--- catalog_viewer.js
+++ catalog_viewer.js
@@ -7,7 +7,19 @@ function loadFromSelect() {
 }
 
 function loadReport(r) {
-  $('#chart').html('Loading data...');
+  var bar = $('<div>', {
+      class:"progress progress-striped active",
+      html: 'Loading data...'
+    })
+    .append($('<div>', {
+      class:"progress-bar",
+      role:"progressbar",
+      aria-valuenow:"100",
+      aria-valuemin:"0",
+      aria-valuemax:"100",
+      style:"width: 100%"
+    }));
+  $('#chart').html(bar);
   $.getJSON('data/'+r+'.json', function(data) {
     diff = data;
     addPie(diff);
