f80db25c648ca64ec155339820e082ee149a1727
Raphaël Pinson <raphink@gmail.com>
2015-05-31 22:37:13 +0200

    missing quotes

--- catalog_viewer.js
+++ catalog_viewer.js
@@ -14,9 +14,9 @@ function loadReport(r) {
     .append($('<div>', {
       class:"progress-bar",
       role:"progressbar",
-      aria-valuenow:"100",
-      aria-valuemin:"0",
-      aria-valuemax:"100",
+      'aria-valuenow':"100",
+      'aria-valuemin':"0",
+      'aria-valuemax':"100",
       style:"width: 100%"
     }));
   $('#chart').html(bar);
