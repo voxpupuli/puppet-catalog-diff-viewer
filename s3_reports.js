5c1114dd6a9f62db967d4cc772ebff1a548fbe21
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-05 15:51:50 +0100

    Cleanup S3 support code

--- s3_reports.js
+++ s3_reports.js
@@ -1,18 +1,15 @@
-AWS.config.update({accessKeyId: s3_access_key, secretAccessKey: s3_secret_key});
-
-var s3Bucket = new AWS.S3({
-    params: {
-      Bucket: s3_bucketName
-    }
-});
-
-var sites = new Object();
-
 function listS3Reports() {
+  AWS.config.update({accessKeyId: s3_access_key, secretAccessKey: s3_secret_key});
+  var s3Bucket = new AWS.S3({
+      params: {
+        Bucket: s3_bucketName
+      }
+  });
   s3Bucket.listObjects(function(err, data) {
     if (err) {
       console.log(err);
     } else {
+      var sites = new Object();
       for (i=0; i<data.Contents.length; i++) {
         var report = data.Contents[i].Key;
         var split = report.split('/');
@@ -22,12 +19,12 @@ function listS3Reports() {
         sites[site][reportName] = report;
       }
 
-      generateReportsMenu(sites);
+      generateS3ReportsMenu(s3Bucket, sites);
     }
   });
 }
 
-function generateReportsMenu(sites) {
+function generateS3ReportsMenu(bucket, sites) {
   siteNames = Object.keys(sites).sort();
   reportsList = $('#reports-list');
   reportsList.html('');
@@ -39,16 +36,17 @@ function generateReportsMenu(sites) {
       var report = sites[site][reportName];
       reportsList.append($('<li>').append($('<a>', {
         id: report,
-        href: "javascript:loadS3Report('"+reportName+"', '"+report+"')",
         html: "<span class='badge'>"+site+"</span> "+reportName
-      })));
+      }).on("click", $.proxy(function(bucket, reportName, report) {
+        loadS3Report(bucket, reportName, report)
+      }, null, bucket, reportName, report))));
     }
   }
 }
 
-function loadS3Report(name, key) {
+function loadS3Report(bucket, name, key) {
   console.log("Loading "+name);
-  s3Bucket.getObject({ Key: key }, function(err, data) {
+  bucket.getObject({ Key: key }, function(err, data) {
     if (err) {
       console.log(err);
     } else {
