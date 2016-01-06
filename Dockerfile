8ef419433318560030a506e113fc1b5ca5fc0a35
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-01-06 09:22:41 +0100

    Add Dockerfile to project

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM tianon/apache2
+
+MAINTAINER raphael.pinson@camptocamp.com
+
+RUN apt-get update \
+  && apt-get install -y git \
+  && rm -rf /var/lib/apt/lists/*
+
+RUN git clone https://github.com/camptocamp/puppet-catalog-diff-viewer.git /var/www/html/catalog_diff
---
+++ spec/Dockerfile_spec.rb
@@ -0,0 +1,16 @@
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    # See https://github.com/swipely/docker-api/issues/106
+    Excon.defaults[:write_timeout] = 1000
+    Excon.defaults[:read_timeout] = 1000
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+    set :docker_container_create_options, { "Privileged" => true }
+  end
+end
