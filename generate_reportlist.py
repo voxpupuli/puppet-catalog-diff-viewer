9bb12d7559b73a84e9ea8526eb68ca7e6012c3ae
Carsten Thiel <cat@frejbow.net>
2016-01-06 19:13:27 +0100

    add python script to autogenerate reportlist.json

---
+++ generate_reportlist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import json
+from os import listdir, path
+maindir = path.dirname(path.abspath(__file__))
+datadir = path.join(maindir,'data')
+datafiles = [f for f in listdir(datadir) if path.isfile(path.join(datadir, f))]
+datadict = {}
+for f in datafiles:
+    if f.endswith('.json'):
+        f = f[:-5]
+        datadict.update({f:f})
+with open(path.join(maindir,'reportlist.json'), 'w') as reportlist:
+    json.dump(datadict, reportlist, indent=4, sort_keys=True)
+
