d01adec96e50f307cd86d76a59cf7489bf42a38a
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-02-23 15:40:44 +0100

    Fix path in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if ! [ -z $S3_BUCKET ]; then
-  cat << EOF > /var/www/html/catalog_diff/s3_credentials.js
+if test -n "$S3_BUCKET" ; then
+  cat << EOF > /usr/share/nginx/html/catalog_diff/s3_credentials.js
 var s3_bucketName = '${S3_BUCKET}';
 var s3_access_key = '${S3_ACCESS_KEY}';
 var s3_secret_key = '${S3_SECRET_KEY}';
